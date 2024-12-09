6413be2dce0278a3ddb66f9aa4ba3530fa6e8b77
dhanushch123 <147328447+dhanushch123@users.noreply.github.com>
2024-12-09 16:15:01 +0530

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,31 @@
+package Othello;
+
+public class Player {
+	private String name;
+	private char symbol;
+	
+	public Player(String name,char symbol)
+	{
+		setName(name);
+		setSymbol(symbol);
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public void setSymbol(char symbol)
+	{
+		if(symbol != '\0')
+		{
+			this.symbol = symbol;
+		}
+	}
+	public String getName()
+	{
+		return this.name;
+	}
+	public char getSymbol()
+	{
+		return this.symbol;
+	}
+}
